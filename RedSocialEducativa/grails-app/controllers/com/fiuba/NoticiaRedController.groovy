840a628f679548d761b5abac9947a03e0c919560
pablomagnaghi <pablomagnaghi@gmail.com>
2014-01-15 21:33:33 +0000

    Spring 1: empiezo a tocar adm noticias cartelera

--- RedSocialEducativa/grails-app/controllers/com/fiuba/NoticiaRedController.groovy
+++ RedSocialEducativa/grails-app/controllers/com/fiuba/NoticiaRedController.groovy
@@ -1,9 +1,107 @@
 package com.fiuba
 
+import static org.springframework.http.HttpStatus.*
+
+//import grails.transaction.Transactional
+
+//@Transactional(readOnly = true)
+
 import org.springframework.security.access.annotation.Secured
 
 @Secured('permitAll')
 class NoticiaRedController {
 
-    static scaffold = true
+    //static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond NoticiaRed.list(params), model:[noticiaRedInstanceCount: NoticiaRed.count()]
+    }
+
+    def show(NoticiaRed noticiaRedInstance) {
+        respond noticiaRedInstance
+    }
+
+    def create() {
+        respond new NoticiaRed(params)
+    }
+
+    // @Transactional
+    def save(NoticiaRed noticiaRedInstance) {
+        if (noticiaRedInstance == null) {
+            notFound()
+            return
+        }
+
+        if (noticiaRedInstance.hasErrors()) {
+            respond noticiaRedInstance.errors, view:'create'
+            return
+        }
+
+        noticiaRedInstance.save flush:true
+
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'noticiaRedInstance.label', default: 'NoticiaRed'), noticiaRedInstance.id])
+                redirect noticiaRedInstance
+            }
+            '*' { respond noticiaRedInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(NoticiaRed noticiaRedInstance) {
+        respond noticiaRedInstance
+    }
+
+    // @Transactional
+    def update(NoticiaRed noticiaRedInstance) {
+        if (noticiaRedInstance == null) {
+            notFound()
+            return
+        }
+
+        if (noticiaRedInstance.hasErrors()) {
+            respond noticiaRedInstance.errors, view:'edit'
+            return
+        }
+
+        noticiaRedInstance.save flush:true
+
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'NoticiaRed.label', default: 'NoticiaRed'), noticiaRedInstance.id])
+                redirect noticiaRedInstance
+            }
+            '*'{ respond noticiaRedInstance, [status: OK] }
+        }
+    }
+
+    // @Transactional
+    def delete(NoticiaRed noticiaRedInstance) {
+
+        if (noticiaRedInstance == null) {
+            notFound()
+            return
+        }
+
+        noticiaRedInstance.delete flush:true
+
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'NoticiaRed.label', default: 'NoticiaRed'), noticiaRedInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'noticiaRedInstance.label', default: 'NoticiaRed'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
 }
--- RedSocialEducativa/grails-app/views/red/configuracion.gsp
+++ RedSocialEducativa/grails-app/views/red/configuracion.gsp
@@ -12,11 +12,11 @@
 				<li><g:link class="list" action="principal">
 					<g:message code="Pagina inicial" args="[entityName]" /></g:link></li>
 				<li><g:link class="create" action="general" controller="administrador">
-					<g:message code="Volver" args="[entityName]" /></g:link></li>
+					<g:message code="Volver a tareas administrativas" args="[entityName]" /></g:link></li>
 			</ul>
 		</div>
 		<div id="edit-red" class="content scaffold-edit" role="main">
-			<h1><g:message code="default.edit.label" args="[entityName]" /></h1>
+			<h1><g:message code="Configuracion de la Red" /></h1>
 			<g:if test="${flash.message}">
 			<div class="message" role="status">${flash.message}</div>
 			</g:if>
