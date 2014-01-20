93e5cee40f105282ea863e913b7ceb3efceddd23
pany100 <pany100@499c94dd-88f1-7751-e8c4-0cabe6a618b3>
2014-01-20 22:55:53 +0000

    Enviar Mensaje

--- RedSocialEducativa/grails-app/services/com/mensajeria/MensajeService.groovy
+++ RedSocialEducativa/grails-app/services/com/mensajeria/MensajeService.groovy
@@ -1,5 +1,6 @@
 package com.mensajeria
 
+import com.fiuba.Usuario
 import grails.transaction.Transactional
 
 @Transactional
@@ -19,4 +20,19 @@ class MensajeService {
 		}
 		nuevaConversacion.save()
     }
+	
+	def sendMessage(String para, String asunto, String texto, Usuario emisor, Usuario receptor, Hilo hilo){
+		def mensaje = new Mensaje(emisor: emisor, receptor: receptor, asunto: asunto,
+			cuerpo: texto, fecha : new Date())
+		mensaje.hilo = hilo;
+		def carpeta = Carpeta.findByNombreAndUsuario("Escritorio", receptor)
+		def nuevaConversacion = new Conversacion(padre: carpeta, hilo: hilo)
+		nuevaConversacion.addToMensajes(mensaje)
+		if (!mensaje.save()) {
+			mensaje.errors.each {
+				println it
+			}
+		}
+		nuevaConversacion.save()
+	}
 }
