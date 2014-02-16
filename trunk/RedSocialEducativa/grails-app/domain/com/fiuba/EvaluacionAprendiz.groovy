bd8ee7304e8b892c981519566810c3e218021748
pablomagnaghi@gmail.com <pablomagnaghi@gmail.com@499c94dd-88f1-7751-e8c4-0cabe6a618b3>
2014-02-16 23:32:06 +0000

    Prueba de commit

--- trunk/RedSocialEducativa/grails-app/domain/com/fiuba/EvaluacionAprendiz.groovy
+++ trunk/RedSocialEducativa/grails-app/domain/com/fiuba/EvaluacionAprendiz.groovy
@@ -2,8 +2,11 @@ package com.fiuba
 
 class EvaluacionAprendiz {
 
+	Integer nota
+	
 	static belongsTo = [aprendiz: Aprendiz, evaluacion: Evaluacion]
 	
     static constraints = {
+		nota nulable: true, min:0
     }
 }
